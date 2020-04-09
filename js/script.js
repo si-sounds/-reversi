23399c101b0e88453f168c3937b2116680cf4568
si-sounds <si_07sounds@yahoo.co.jp>
2020-04-09 20:18:42 +0900

    0409

--- js/script.js
+++ js/script.js
@@ -1,15 +1,3 @@
-document.getElementById("uploadKuro").addEventListener('change', function(e){
-    var file = e.target.files[0];
-    var blobUrl = window.URL.createObjectURL(file);
-    k = blobUrl;
-	reset();
-});
-document.getElementById("uploadSiro").addEventListener('change', function(e){
-    var file = e.target.files[0];
-    var blobUrl = window.URL.createObjectURL(file);
-    s = blobUrl;
-	reset();
-});
 const n = "pic/dummy.png";	//no disc
 
 var turn = k; //黒から
